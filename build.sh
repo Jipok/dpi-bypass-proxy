7d6b2f8864dcc64b0691a7dffe928d6014abb8d4
Jipok <github@jipok.ru>
2024-08-07 22:53:10 +0500

    Support mipsle

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 GOOS=linux GOARCH=amd64 go build -o dpi-bypass-proxy_amd64 -ldflags "-s -w" && upx dpi-bypass-proxy_amd64
 GOOS=linux GOARCH=arm go build -o dpi-bypass-proxy_arm -ldflags "-s -w" && upx dpi-bypass-proxy_arm
 GOOS=linux GOARCH=arm64 go build -o dpi-bypass-proxy_arm64 -ldflags "-s -w" && upx dpi-bypass-proxy_arm64
-GOOS=linux GOARCH=mips go build -o dpi-bypass-proxy_mips -ldflags "-s -w" && upx dpi-bypass-proxy_mips
+GOOS=linux GOARCH=mips GOMIPS=softfloat go build -o dpi-bypass-proxy_mips -ldflags "-s -w" && upx dpi-bypass-proxy_mips
+GOOS=linux GOARCH=mipsle GOMIPS=softfloat go build -o dpi-bypass-proxy_mipsle -ldflags "-s -w" && upx dpi-bypass-proxy_mipsle
